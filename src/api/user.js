6d2766426d0fba68eda983ca65c3c1a827c4b02f
zhangbo <854008949@qq.com>
2020-01-02 23:26:13 +0800

    20190102

---
+++ src/api/user.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/login',
+    method: 'post',
+    headers:{
+     "content-type": "application/x-www-form-urlencoded",
+    },
+    data
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
