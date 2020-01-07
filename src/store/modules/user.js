fc3a8d570d98c514109eed480045557b399f5029
zhangbo <854008949@qq.com>
2020-01-07 14:33:40 +0800

    20200107

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -35,8 +35,8 @@ const actions = {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
         console.log(data);
-        //commit('SET_TOKEN', data.token)
-        //setToken(data.token)
+        commit('SET_TOKEN', data.token)
+        setToken(data.token)
         resolve()
       }).catch(error => {
         console.log(error);
